b551ef9d0c3b48b7e1a2e70b5d47c9f10c34193f
Wouter van Heeswijk <woutervh@gmail.com>
2018-08-13 22:33:47 +0200

    Better typing

--- r-tree/src/index.ts
+++ r-tree/src/index.ts
@@ -1,47 +1,38 @@
 type Bounds = number[];
 
-type TreeNode<Pointer> = Branch<Pointer> | Leaf<Pointer>;
-
 interface Bounded {
     bounds: Bounds;
 }
 
-interface BranchChild<Pointer> {
-    bounds: Bounds;
-    node: TreeNode<Pointer>;
+interface LeafEntry<Data> extends Bounded {
+    data: Data;
 }
 
-interface Branch<Pointer> {
-    type: 'branch';
-    children: BranchChild<Pointer>[];
-}
-
-type Child<Pointer> = BranchChild<Pointer> | LeafChild<Pointer>;
-
-interface Parent<Pointer> {
-    bounds: Bounds;
-    children: Child<Pointer>[];
-}
-
-interface LeafChild<Pointer> {
-    bounds: Bounds;
-    entry: Pointer;
-}
-
-interface Leaf<Pointer> {
+interface LeafNode<Data> {
     type: 'leaf';
-    children: LeafChild<Pointer>[];
+    entries: LeafEntry<Data>[];
 }
 
-interface LeafPath<Pointer> {
-    parents: Branch<Pointer>[];
-    leaf: Leaf<Pointer>;
+interface BranchEntry<Data> extends Bounded {
+    child: TreeNode<Data>;
 }
 
-interface Tree<Pointer> {
-    root: TreeNode<Pointer>;
+interface BranchNode<Data> {
+    type: 'branch';
+    entries: BranchEntry<Data>[];
+}
+
+type TreeNode<Data> = BranchNode<Data> | LeafNode<Data>;
+
+interface Tree<Data> {
+    root: TreeNode<Data>;
     dimensions: number;
     maximumEntries: number;
+}
+
+interface LeafPath<Data> {
+    branches: BranchNode<Data>[];
+    leaf: LeafNode<Data>;
 }
 
 function overlaps(a: Bounds, b: Bounds, dimensions: number): boolean {
@@ -79,49 +70,52 @@ function enclose(boundsList: Bounds[], dimensions: number): Bounds {
     return bounds;
 }
 
-function search<Pointer>(tree: Tree<Pointer>, root: TreeNode<Pointer>, bounds: Bounds): Pointer[] {
-    if (root.type === 'leaf') {
-        return root.children
-            .filter((child) => overlaps(bounds, child.bounds, tree.dimensions))
-            .map((child) => child.entry);
+function search<Data>(tree: Tree<Data>, node: TreeNode<Data>, bounds: Bounds): Data[] {
+    if (node.type === 'leaf') {
+        return node.entries
+            .filter((entry) => overlaps(bounds, entry.bounds, tree.dimensions))
+            .map((entry) => entry.data);
     } else {
-        return root.children
-            .filter((child) => overlaps(bounds, child.bounds, tree.dimensions))
-            .map((child) => search(tree, child.node, bounds))
-            .reduce((accumulator, pointers) => accumulator.concat(pointers), []);
+        return node.entries
+            .filter((entry) => overlaps(bounds, entry.bounds, tree.dimensions))
+            .map((entry) => search(tree, entry.child, bounds))
+            .reduce((accumulator, data) => accumulator.concat(data), []);
     }
 }
 
-function chooseLeaf<Pointer>(tree: Tree<Pointer>, bounds: Bounds): LeafPath<Pointer> {
-    const parents: Branch<Pointer>[] = [];
-    let node: TreeNode<Pointer> = tree.root;
+function chooseLeaf<Data>(tree: Tree<Data>, bounds: Bounds): LeafPath<Data> {
+    const path: BranchNode<Data>[] = [];
+    let node: TreeNode<Data> = tree.root;
     while (node.type !== 'leaf') {
-        parents.push(node);
         // Choose subtree
-        let selectedChild = node.children[0];
-        let minArea = area(selectedChild.bounds, tree.dimensions);
-        let minEnlargement = area(combine(selectedChild.bounds, bounds, tree.dimensions), tree.dimensions) - minArea;
-        for (let i = 1; i < node.children.length; i++) {
-            const child = node.children[i];
-            const childArea = area(child.bounds, tree.dimensions);
-            const childEnlargement = area(combine(child.bounds, bounds, tree.dimensions), tree.dimensions) - childArea;
-            if (childEnlargement < minEnlargement || childEnlargement === minEnlargement && childArea < minArea) {
-                selectedChild = child;
-                minArea = childArea;
-                minEnlargement = childEnlargement;
+        let selectedEntry: BranchEntry<Data> | undefined = undefined;
+        let minArea = Number.POSITIVE_INFINITY;
+        let minEnlargement = Number.POSITIVE_INFINITY;
+        for (let i = 0; i < node.entries.length; i++) {
+            const entry = node.entries[i];
+            const entryArea = area(entry.bounds, tree.dimensions);
+            const entryEnlargement = area(combine(entry.bounds, bounds, tree.dimensions), tree.dimensions) - entryArea;
+            if (entryEnlargement < minEnlargement || entryEnlargement === minEnlargement && entryArea < minArea) {
+                selectedEntry = entry;
+                minArea = entryArea;
+                minEnlargement = entryEnlargement;
             }
         }
-        node = selectedChild.node;
+        if (selectedEntry === undefined) {
+            throw new Error('No entry was selected.');
+        }
+        path.push(node);
+        node = selectedEntry.child;
     }
-    return { parents, leaf: node };
+    return { branches: path, leaf: node };
 }
 
-function pickSeeds<Pointer>(tree: Tree<Pointer>, entries: BranchChild<Pointer>[]): [BranchChild<Pointer>, BranchChild<Pointer>];
-function pickSeeds<Pointer>(tree: Tree<Pointer>, entries: LeafChild<Pointer>[]): [LeafChild<Pointer>, LeafChild<Pointer>];
-function pickSeeds<Pointer>(tree: Tree<Pointer>, entries: Child<Pointer>[]): [Child<Pointer>, Child<Pointer>];
-function pickSeeds<Pointer>(tree: Tree<Pointer>, entries: Child<Pointer>[]): [Child<Pointer>, Child<Pointer>] {
+function pickSeeds<Data>(tree: Tree<Data>, entries: LeafEntry<Data>[]): [LeafEntry<Data>, LeafEntry<Data>];
+function pickSeeds<Data>(tree: Tree<Data>, entries: BranchEntry<Data>[]): [BranchEntry<Data>, BranchEntry<Data>];
+function pickSeeds<Data, Entry extends LeafEntry<Data> | BranchEntry<Data>>(tree: Tree<Data>, entries: Entry[]): [Entry, Entry];
+function pickSeeds<Data, Entry extends LeafEntry<Data> | BranchEntry<Data>>(tree: Tree<Data>, entries: Entry[]): [Entry, Entry] {
     let maxWastedArea = Number.NEGATIVE_INFINITY;
-    let pair: [Child<Pointer>, Child<Pointer>] | undefined = undefined;
+    let pair: [Entry, Entry] | undefined = undefined;
     for (let i = 0; i < entries.length; i++) {
         for (let j = 0; j < entries.length; j++) {
             if (i !== j) {
@@ -142,9 +136,12 @@ function pickSeeds<Pointer>(tree: Tree<Pointer>, entries: Child<Pointer>[]): [Ch
     }
 }
 
-function pickNext<Pointer>(tree: Tree<Pointer>, left: Parent<Pointer>, right: Parent<Pointer>, entries: Set<Child<Pointer>>): Child<Pointer> {
+function pickNext<Data>(tree: Tree<Data>, left: Bounded, right: Bounded, entries: Set<LeafEntry<Data>>): LeafEntry<Data>;
+function pickNext<Data>(tree: Tree<Data>, left: Bounded, right: Bounded, entries: Set<BranchEntry<Data>>): BranchEntry<Data>;
+function pickNext<Data, Entry extends LeafEntry<Data> | BranchEntry<Data>>(tree: Tree<Data>, left: Bounded, right: Bounded, entries: Set<Entry>): Entry;
+function pickNext<Data, Entry extends LeafEntry<Data> | BranchEntry<Data>>(tree: Tree<Data>, left: Bounded, right: Bounded, entries: Set<Entry>): Entry {
     let maxDifference = Number.NEGATIVE_INFINITY;
-    let selectedNode: Child<Pointer> | undefined = undefined;
+    let selectedNode: Entry | undefined = undefined;
     const leftArea = area(left.bounds, tree.dimensions);
     const rightArea = area(right.bounds, tree.dimensions);
     for (const entry of entries) {
@@ -162,29 +159,30 @@ function pickNext<Pointer>(tree: Tree<Pointer>, left: Parent<Pointer>, right: Pa
     }
 }
 
-function quadraticSplit<Pointer>(tree: Tree<Pointer>, node: Leaf<Pointer>): [Leaf<Pointer>, Leaf<Pointer>];
-function quadraticSplit<Pointer>(tree: Tree<Pointer>, node: Branch<Pointer>): [Branch<Pointer>, Branch<Pointer>];
-function quadraticSplit<Pointer>(tree: Tree<Pointer>, node: TreeNode<Pointer>): [TreeNode<Pointer>, TreeNode<Pointer>] {
-    const seeds = pickSeeds(tree, node.children);
-    const left: TreeNode<Pointer> & Bounded = {
-        type: node.type,
+function quadraticSplit<Data>(tree: Tree<Data>, entries: LeafEntry<Data>[]): [LeafEntry<Data>[], LeafEntry<Data>[]];
+function quadraticSplit<Data>(tree: Tree<Data>, entries: BranchEntry<Data>[]): [BranchEntry<Data>[], BranchEntry<Data>[]];
+function quadraticSplit<Data, Entry extends LeafEntry<Data> | BranchEntry<Data>>(tree: Tree<Data>, entries: Entry[]): [Entry[], Entry[]] {
+    interface Split extends Bounded {
+        entries: Entry[];
+    }
+
+    const seeds = pickSeeds(tree, entries);
+    const left: Split = {
         bounds: seeds[0].bounds,
-        children: [seeds[0]]
+        entries: [seeds[0]]
     };
-    const right: TreeNode<Pointer> & Bounded = {
-        type: node.type,
+    const right: Split = {
         bounds: seeds[1].bounds,
-        children: [seeds[1]]
+        entries: [seeds[1]]
     };
-    const nodeChildren: Child<Pointer>[] = node.children;
-    const entries = new Set(nodeChildren.filter((entry) => entry !== seeds[0] && entry !== seeds[1]));
-    while (entries.size >= 1) {
-        const next = pickNext(tree, left, right, entries);
+    const candidates = new Set(entries.filter((entry) => entry !== seeds[0] && entry !== seeds[1]));
+    while (candidates.size >= 1) {
+        const next = pickNext(tree, left, right, candidates);
         const leftAreaBefore = area(left.bounds, tree.dimensions)
         const rightAreaBefore = area(right.bounds, tree.dimensions)
         const enlargementLeft = area(combine(left.bounds, next.bounds, tree.dimensions), tree.dimensions) - leftAreaBefore;
         const enlargementRight = area(combine(right.bounds, next.bounds, tree.dimensions), tree.dimensions) - rightAreaBefore;
-        let addTo: Parent<Pointer>;
+        let addTo: Split;
         if (enlargementLeft < enlargementRight) {
             addTo = left;
         } else if (enlargementLeft > enlargementRight) {
@@ -195,33 +193,33 @@ function quadraticSplit<Pointer>(tree: Tree<Pointer>, node: TreeNode<Pointer>): 
             } else if (leftAreaBefore > rightAreaBefore) {
                 addTo = right;
             } else {
-                if (left.children.length < right.children.length) {
+                if (left.entries.length < right.entries.length) {
                     addTo = left;
                 } else {
                     addTo = right;
                 }
             }
         }
-        addTo.children.push(next);
+        addTo.entries.push(next);
         addTo.bounds = combine(addTo.bounds, next.bounds, tree.dimensions);
-        entries.delete(next);
+        candidates.delete(next);
     }
-    return [left, right];
+    return [left.entries, right.entries];
 }
 
-function adjustTree<Pointer>(tree: Tree<Pointer>, path: LeafPath<Pointer>, n1: TreeNode<Pointer>, n2?: TreeNode<Pointer>) {
+function adjustTree<Data>(tree: Tree<Data>, path: LeafPath<Data>, n1: TreeNode<Data>, n2?: TreeNode<Data>) {
     while (tree.root !== n1) {
-        const parent = path.parents.pop();
+        const parent = path.branches.pop();
         if (parent === undefined) {
             throw new Error('Parent path too short.');
         }
-        const n1Index = parent.children.findIndex((child) => child.node === n1);
-        const n1Children: Bounded[] = n1.children;
-        parent.children[n1Index].bounds = combine(parent.children[n1Index].bounds, enclose(n1Children.map((child) => child.bounds), tree.dimensions), tree.dimensions);
+        const n1Index = parent.entries.findIndex((entry) => entry.child === n1);
+        const n1Entries: Bounded[] = n1.entries;
+        parent.entries[n1Index].bounds = combine(parent.entries[n1Index].bounds, enclose(n1Entries.map((entry) => entry.bounds), tree.dimensions), tree.dimensions);
         if (n2) {
-            const n2Children: Bounded[] = n2.children;
-            const entry: BranchChild<Pointer> = {
-                bounds: enclose(n2Children.map((child) => child.bounds), tree.dimensions),
+            const n2Entries: Bounded[] = n2.entries;
+            const entry: BranchChild<Data> = {
+                bounds: enclose(n2Entries.map((entry) => entry.bounds), tree.dimensions),
                 node: n2
             };
             if (parent.children.length < tree.maximumEntries) {
@@ -238,22 +236,22 @@ function adjustTree<Pointer>(tree: Tree<Pointer>, path: LeafPath<Pointer>, n1: T
     }
 }
 
-function insert<Pointer>(tree: Tree<Pointer>, child: LeafChild<Pointer>) {
-    const path = chooseLeaf(tree, child.bounds);
-    if (path.leaf.children.length < tree.maximumEntries) {
-        path.leaf.children.push(child);
+function insert<Data>(tree: Tree<Data>, entry: LeafEntry<Data>) {
+    const path = chooseLeaf(tree, entry.bounds);
+    if (path.leaf.entries.length < tree.maximumEntries) {
+        path.leaf.entries.push(entry);
     } else {
-        path.leaf.children.push(child);
-        const split = quadraticSplit(tree, path.leaf);
+        path.leaf.entries.push(entry);
+        const split = quadraticSplit(tree, path.leaf.entries);
     }
 }
 
 const myTree: Tree<string> = {
     root: {
         type: 'leaf',
-        children: [{
+        entries: [{
             bounds: [0, 1, 0, 1],
-            entry: 'R1'
+            data: 'R1'
         }]
     },
     dimensions: 2,
